a2d7d3158a490dbd5bfb473e2571ad6c3d47db1d
Vanya Delova <happy.vanya.delova@gmail.com>
2018-10-31 16:26:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import TodoList from './ToDoList'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.sate = {
+      items: [],
+      currentItem: {text:'', key:''},
+    }
+  }
+  handleInput = e => {
+    console.log('Hello Input')
+  }
+  addItem = () => {
+    console.log('Hello Add Item')
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <TodoList addItem={this.addItem} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
